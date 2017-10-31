80a37c39c56ad901da55bd689906b9e5758ae318
Alex <alexander.ba@gmail.com>
2017-10-31 14:28:37 +0200

    add CW Users

---
+++ src/cw03/oop/com/userlogin/UserApp.java
@@ -0,0 +1,15 @@
+package cw03.oop.com.userlogin;
+
+import cw03.oop.com.userlogin.UserHandler;
+
+public class UserApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		UserHandler users = new UserHandler(3);
+		users.addUser("Billi Gaitss", "12345");
+		users.printUser(0);
+		System.out.println(users.printUser(0));
+	}
+
+}
---
+++ src/cw03/oop/com/userlogin/UserHandler.java
@@ -0,0 +1,38 @@
+package cw03.oop.com.userlogin;
+
+import cw03.oop.com.userlogin.User;
+
+public class UserHandler {
+	private User[] users;
+		
+	
+    public UserHandler(int maximumUsers) {
+		this.users = new User[maximumUsers];
+	}
+   
+    public boolean addUser(String username, String password) {
+        int index = nextIndex();
+        if (index >= 0) {
+            User newUser = new User(username, password);
+            this.users[index] = newUser;
+            return true;
+        }
+        return false;
+	}
+    
+    public String printUser(int index) {
+    		return this.users[index].toString();
+    }
+	/**
+     * @return the next available index in the products array or -1 if the array is full
+     */
+    private int nextIndex() {
+        for (int i = 0; i < this.users.length; i++) {
+            if (users[i] == null) {
+                return i;
+            }
+        }
+        return -1; // no place available
+    }
+	
+}
